0d28057795759a05c43d75a44af3940bc3f5b44d
TBot <99534248+T-S-T-T@users.noreply.github.com>
2025-09-14 04:20:09 +1000

    Update MemberMovement1.cs

--- Assets/SceneSpecific/Playground/ClusterVisual/Core1/MemberMovement1.cs
+++ Assets/SceneSpecific/Playground/ClusterVisual/Core1/MemberMovement1.cs
@@ -3,33 +3,19 @@ using UnityEngine;
 public class MemberMovement1 : MonoBehaviour
 {
     [HideInInspector]
-    public GameObject core;           // Assigned by your spawner
+    public GameObject core;
+    public float maxRange;
 
-    private ClusterVisual1 cluster;   // Holds maxRange (and spacing, if you add it)
-    private Vector3 offset;           // Initial grid offset from core
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Cache the core’s ClusterVisual1 for range (and spacing) data
-        cluster = core.GetComponent<ClusterVisual1>();
-
-        // Compute and store the member’s original offset on the grid
-        offset = transform.position - core.transform.position;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Fast check using squared magnitudes
-        float r = cluster.maxRange;
-        if ((transform.position - core.transform.position).sqrMagnitude > r * r)
-        {
-            // Invert the stored offset to wrap to the opposite side
-            offset = -offset;
-
-            // Teleport to the new wrapped-around position
-            transform.position = core.transform.position + offset;
-        }
-
-        // You can still use maxRange elsewhere in this script if needed
+        ClusterVisual1 clusterVisual1 = core.GetComponent<ClusterVisual1>();
+        maxRange = clusterVisual1.maxRange;
     }
 }
