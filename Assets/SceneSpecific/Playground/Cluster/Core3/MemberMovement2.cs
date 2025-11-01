3f3c01d7809659e539c0ce2c88fa562e25e696f5
TBot <99534248+T-S-T-T@users.noreply.github.com>
2025-11-01 18:06:13 +1100

    Update MemberMovement2.cs

--- Assets/SceneSpecific/Playground/Cluster/Core3/MemberMovement2.cs
+++ Assets/SceneSpecific/Playground/Cluster/Core3/MemberMovement2.cs
@@ -5,14 +5,21 @@ public class MemberMovement2 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public GameObject core;
+
+    public Vector3 node;
     void Start()
     {
-        Debug.Log(core.name);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void MoveToNode()
+    {
+        transform.position = node;
     }
 }
