f4c09b3b8d36ca9911f0709f6b2df002831f6d69
TBot <tungtincan@gmail.com>
2025-11-06 10:19:41 +0700

    Update MemberMovement3.cs

--- Assets/SceneSpecific/Playground/Cluster/Core4/MemberMovement3.cs
+++ Assets/SceneSpecific/Playground/Cluster/Core4/MemberMovement3.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class MemberMovement3 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f; // movement speed
+    private Vector3 direction;
+    private Transform meshTransform;
+
     void Start()
     {
-        
+        // Pick a random direction in 3D space
+        direction = Random.onUnitSphere;
+
+        // Cache the child called "Mesh"
+        meshTransform = transform.Find("Mesh");
+        if (meshTransform == null)
+        {
+            Debug.LogError("Child 'Mesh' not found! Make sure the GameObject has a child named 'Mesh'.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the boid along its direction
+        transform.position += direction * speed * Time.deltaTime;
+
+        // Rotate the Mesh child to face the direction
+        if (meshTransform != null)
+        {
+            meshTransform.rotation = Quaternion.LookRotation(direction);
+        }
     }
 }
